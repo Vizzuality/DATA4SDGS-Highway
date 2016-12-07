a3e4cb738d9aefaacc68cd212dca3d0dc735e887
Rodrigo Solís <rodrigo.solis@vizzuality.com>
2016-12-07 16:50:22 +0100

    debounce search bar + featured datasets loaded

---
+++ app/scripts/components/DatasetList/index.vue
@@ -0,0 +1,25 @@
+<template src="./dataset-list-template.html"> </template>
+
+<style lang="scss" src="./dataset-list-style.scss"> </style>
+
+<script>
+import DatasetComponent from 'components/Dataset';
+
+export default{
+  name: 'dataset-list-component',
+  props: {
+    datasets: {
+      type: Array
+    },
+  },
+  computed: {
+  },
+  data() {
+    return {
+    };
+  },
+  components: {
+    DatasetComponent,
+  },
+};
+</script>
