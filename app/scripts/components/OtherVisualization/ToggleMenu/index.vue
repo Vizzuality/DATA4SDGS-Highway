fdcdc52c3f032dfb1ac65c7d0dc06df1027ccae3
Óscar Ferrándiz <oscar.ferrandiz@vizzuality.com>
2017-01-13 12:42:32 +0100

    sort table

--- app/scripts/components/OtherVisualization/ToggleMenu/index.vue
+++ app/scripts/components/OtherVisualization/ToggleMenu/index.vue
@@ -8,7 +8,12 @@
     name: 'toggle-menu',
     data() {
       return {
-        closed: false
+        closed: false,
+        sort: {
+          field: 'water',
+          direction: 1
+        },
+        basinsList: []
       };
     },
     computed: {
@@ -19,6 +24,31 @@
     methods: {
       toggle() {
         this.closed = !this.closed;
+      },
+      setSortField(field) {
+        this.sort.direction = field === this.sort.field ? -this.sort.direction : 1;
+        this.sort.field = field;
+      },
+      sortBasins() {
+        const aux = this.basins.slice(0);
+        aux.sort((a, b) => { // eslint-disable-line
+          if (this.sort.direction === 1) {
+            return a[this.sort.field] > b[this.sort.field] ? 1 : -1;
+          }
+          return a[this.sort.field] > b[this.sort.field] ? -1 : 1;
+        });
+        this.basinsList = aux;
+      }
+    },
+    watch: {
+      basins() {
+        this.sortBasins();
+      },
+      'sort.field': function() { // eslint-disable-line
+        this.sortBasins();
+      },
+      'sort.direction': function() { // eslint-disable-line
+        this.sortBasins();
       }
     }
   };
