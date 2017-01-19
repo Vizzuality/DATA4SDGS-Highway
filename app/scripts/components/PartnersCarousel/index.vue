59b395236e84bf0b4405a2576968f13d3da01527
Óscar Ferrándiz <oscar.ferrandiz@vizzuality.com>
2017-01-19 12:35:27 +0100

    Partners carousel

---
+++ app/scripts/components/PartnersCarousel/index.vue
@@ -0,0 +1,40 @@
+<template src="./template.html"></template>
+<style lang="scss" src="./style.scss"> </style>
+<style lang="scss" src="slick-carousel/slick/slick.scss"> </style>
+<script>
+
+  import Slick from 'vue-slick';
+
+  export default {
+
+    name: 'search-component',
+
+    data() {
+      return {
+        slickOptions: {
+          slidesToShow: 8,
+          slidesToScroll: 1,
+          autoplay: true,
+          autoplaySpeed: 3000,
+          arrows: false,
+        },
+      };
+    },
+
+    components: {
+      Slick
+    },
+
+    // All slick methods can be used too, example here
+    methods: {
+      next() {
+        this.$refs.slick.next();
+      },
+
+      prev() {
+        this.$refs.slick.prev();
+      }
+    }
+
+  };
+</script>
