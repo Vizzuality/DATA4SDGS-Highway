c46d8e6821a237cb2e6ab7d1e92b4d61147950dc
rrequero <rrequero@gmail.com>
2018-02-28 12:45:50 +0100

    layers workaround

--- app/scripts/store/modules/water_risk_layers.js
+++ app/scripts/store/modules/water_risk_layers.js
@@ -1,4 +1,3 @@
-
 /* eslint-disable */
 import {
   SET_WATER_LAYER_SUCCESS,
@@ -11,6 +10,161 @@ import {
 } from '../mutation-types';
 
 const BASE_URL = global.API_BASE_URL;
+
+const DEFAULT_LAYERS = [{
+  'id': '1c56e5c4-f617-4570-90dd-20724ea674b1',//dataset id
+  'name': 'Water transitions',
+  'slug': 'water-transitions',
+  'dataset': '1c56e5c4-f617-4570-90dd-20724ea674b1',
+  'description': 'Water transitions',
+  'application': ['data4sdgs'],
+  'iso': [],
+  'provider': 'cartodb',
+  'userId': '57a0aa1071e394dd32ffe137',
+  'default': true,
+  'protected': false,
+  'env': 'production',
+  'layerConfig': {
+      'account': 'simbiotica',
+      'params_config': [],
+      'sql_config': [],
+      'body': {
+          'maxzoom': 18,
+          'minzoom': 3,
+          'layers': [{
+              'type': 'cartodb',
+              'options': {
+                  'sql': 'Select * from water_final',
+                  'cartocss': '#year1 {raster-opacity:1; raster-scaling:near; raster-colorizer-default-mode:exact; raster-colorizer-default-color:  transparent; raster-colorizer-epsilon:0.11; raster-colorizer-stops: stop(1, #0000dd) stop(2, #22b14c) stop(3, #d12d3e) stop(4, #99d9ea) stop(5, #b5e61d) stop(6, #e6a1aa) stop(7, #ff7f27) stop(8, #ffc90e) stop(9, #7f7f7f) stop(10, #c3c3c3)}',
+                  'cartocss_version': '2.3.0',
+                  'geom_column': 'the_raster_webmercator',
+                  'geom_type': 'raster',
+                  'raster_band': 1
+              }
+          }]
+      }
+  },
+  'legendConfig': {
+      'type': 'basic',
+      'items': [{
+          'name': 'Permanent water body',
+          'color': '#0000dd'
+      }, {
+          'name': 'New permanent',
+          'color': '#22b14c'
+      }, {
+          'name': 'Lost permanent',
+          'color': '#d12d3e'
+      }, {
+          'name': 'Seasonal',
+          'color': '#99d9ea'
+      }, {
+          'name': 'New seasonal',
+          'color': '#b5e61d'
+      }, {
+          'name': 'Lost seasonal',
+          'color': '#e6a1aa'
+      }, {
+          'name': 'Seasonal to permanent',
+          'color': '#ff7f27'
+      }, {
+          'name': 'Permanent to seasonal',
+          'color': '#ffc90e'
+      }, {
+          'name': 'Ephemeral permanent',
+          'color': '#7f7f7f'
+      }, {
+          'name': 'Ephemeral seasonal',
+          'color': '#c3c3c3'
+      }]
+  },
+  'interactionConfig': {},
+  'applicationConfig': {},
+  'staticImageConfig': {},
+  'updatedAt': '2017-01-12T15:40:02.963Z'
+}, {
+  'id': 'bd47397c-b714-4ced-9fe9-d20b0b353e3f', //dataset id
+  'name': 'Population in South Africa',
+  'slug': 'population-in-south-africa',
+  'dataset': 'bd47397c-b714-4ced-9fe9-d20b0b353e3f',
+  'description': 'Population in South Africa',
+  'application': ['data4sdgs'],
+  'iso': [],
+  'provider': 'cartodb',
+  'userId': '57a0aa1071e394dd32ffe137',
+  'default': true,
+  'protected': false,
+  'env': 'production',
+  'layerConfig': {
+      'account': 'simbiotica',
+      'params_config': [],
+      'sql_config': [],
+      'body': {
+          'maxzoom': 18,
+          'minzoom': 3,
+          'layers': [{
+              'type': 'cartodb',
+              'options': {
+                  'sql': 'Select * from pop_well',
+                  'cartocss': '#pop_well {raster-opacity:1; raster-scaling:near; raster-colorizer-default-mode: linear; raster-colorizer-default-color:  transparent; raster-colorizer-epsilon:0.11; raster-colorizer-stops: stop(1, #FFF6CC) stop(20, #FFEE99) stop(70, #E5CC4C) stop(150, #E5C319) stop(300, #B29919) stop(500, #998319) }',
+                  'cartocss_version': '2.3.0',
+                  'geom_column': 'the_raster_webmercator',
+                  'geom_type': 'raster',
+                  'raster_band': 1
+              }
+          }]
+      }
+  },
+  'legendConfig': {
+      'type': 'basic',
+      'items': []
+  },
+  'interactionConfig': {},
+  'applicationConfig': {},
+  'staticImageConfig': {},
+  'updatedAt': '2017-01-12T17:12:12.626Z'
+},{
+  'id': 'ee2c5877-f212-4eb4-a63a-a82ba7e4e168', // dataset id
+  'name': 'Water basins from South Africa',
+  'slug': 'water-basins-from-south-africa',
+  'dataset': 'ee2c5877-f212-4eb4-a63a-a82ba7e4e168',
+  'description': 'water basins  from south africa',
+  'application': ['data4sdgs'],
+  'iso': [],
+  'provider': 'cartodb',
+  'userId': '57a0aa1071e394dd32ffe137',
+  'default': false,
+  'protected': false,
+  'env': 'production',
+  'layerConfig': {
+      'account': 'simbiotica',
+      'params_config': [],
+      'sql_config': [],
+      'body': {
+          'maxzoom': 18,
+          'minzoom': 3,
+          'layers': [{
+              'type': 'cartodb',
+              'options': {
+                  'sql': 'SELECT min(cartodb_id) cartodb_id, st_union(the_geom_webmercator) the_geom_webmercator, basinid, basin_name  FROM water_basin_sa group by basinid, basin_name',
+                  'cartocss': '#water_basin_sa {    polygon-opacity: 0;    line-color: rgba(212,18,89,1);    line-width: 1;    line-opacity: 1; }',
+                  'cartocss_version': '2.3.0'
+              }
+          }]
+      }
+  },
+  'legendConfig': {
+      'type': 'basic',
+      'items': [{
+          'name': 'Extremely high (>80%)',
+          'color': '#D4155B'
+      }]
+  },
+  'interactionConfig': {},
+  'applicationConfig': {},
+  'staticImageConfig': {},
+  'updatedAt': '2017-01-12T15:12:11.760Z'
+}]
 
 export default {
   /*
@@ -63,39 +217,47 @@ export default {
     ACTIONS
   */
   actions: {
+
     // Fetch layers from api
-    getWaterRiskLayers({ commit }) {
+    getWaterRiskLayers({
+      commit
+    }) {
+
       commit(SET_WATER_LAYER_LOADING, true);
-      fetch(new Request(`${BASE_URL}/dataset?application=data4sdgs&includes=vocabulary,layer&vocabulary[legacy]=data4sdgs-vizz2&cache=expire`))
-      .then((response) => {
-        if (response.ok) return response.json();
-        throw new Error(response.statusText);
-      })
-      .then((datasets) => {
-        const layers = [];
-        const lsActives = [];
-        datasets.data.forEach((dataset) => {
-          const l = Object.assign({}, dataset.attributes.layer[0].attributes, { id: dataset.id });
-          layers.push(l);
-          lsActives.push(dataset.id);
+      fetch(new Request(`${BASE_URL}/dataset?application=data4sdgs&includes=vocabulary&vocabulary[legacy]=data4sdgs-vizz2&cache=expire`))
+        .then((response) => {
+          if (response.ok) return response.json();
+          throw new Error(response.statusText);
+        })
+        .then((datasets) => {
+          const layers = [];
+          const lsActives = [];
+          DEFAULT_LAYERS.forEach(layer => {
+            layers.push(layer);
+            lsActives.push(layer.id);
+          })
+
+          // Fetch from server ok -> Commit layers
+          commit(SET_WATER_LAYER_SUCCESS, layers);
+          commit(SET_WATER_LAYERS_ACTIVE, lsActives);
+          commit(SET_WATER_LAYER_LOADING, false);
+        })
+        .catch((err) => {
+          // Fetch from server ko -> Dispatch error
+          commit(SET_WATER_LAYER_ERROR, err);
+          commit(SET_WATER_LAYER_LOADING, false);
         });
-        // Fetch from server ok -> Commit layers
-        commit(SET_WATER_LAYER_SUCCESS, layers);
-        commit(SET_WATER_LAYERS_ACTIVE, lsActives);
-        commit(SET_WATER_LAYER_LOADING, false);
-      })
-      .catch((err) => {
-        // Fetch from server ko -> Dispatch error
-        commit(SET_WATER_LAYER_ERROR, err);
-        commit(SET_WATER_LAYER_LOADING, false);
-      });
     },
 
-    setActiveLayers({ commit }, layers) {
+    setActiveLayers({
+      commit
+    }, layers) {
       commit(SET_WATER_LAYERS_ACTIVE, layers);
     },
 
-    getWaterBasins({ commit }) {
+    getWaterBasins({
+      commit
+    }) {
       // TODO: this should be a dataset insted of a requests to carto
       const url = `https://simbiotica.carto.com/api/v2/sql?q=with r as (SELECT sum(population) pop, basin_name, basinid,
       sum((transition_types::json->>'3')::numeric*pixel_area/10000) water_area_ha FROM water_basin_sa group by basin_name,
@@ -104,19 +266,19 @@ export default {
 
       commit(SET_BASINS_LOADING, true);
       fetch(new Request(url))
-      .then((response) => {
-        if (response.ok) return response.json();
-        throw new Error(response.statusText);
-      })
-      .then((basins) => {
-        commit(SET_BASINS_SUCCESS, basins.rows);
-        commit(SET_BASINS_LOADING, false);
-      })
-      .catch((err) => {
-        // Fetch from server ko -> Dispatch error
-        commit(SET_BASINS_ERROR, err);
-        commit(SET_BASINS_LOADING, false);
-      });
+        .then((response) => {
+          if (response.ok) return response.json();
+          throw new Error(response.statusText);
+        })
+        .then((basins) => {
+          commit(SET_BASINS_SUCCESS, basins.rows);
+          commit(SET_BASINS_LOADING, false);
+        })
+        .catch((err) => {
+          // Fetch from server ko -> Dispatch error
+          commit(SET_BASINS_ERROR, err);
+          commit(SET_BASINS_LOADING, false);
+        });
     }
   },
 
@@ -124,7 +286,9 @@ export default {
     GETTERS
   */
   getters: {
-    getActiveLayers({ layers }) {
+    getActiveLayers({
+      layers
+    }) {
       return layers.list.filter(layer => layers.active.includes(layer.id));
     }
   }
