d1a8285a9df996051799aa6da2438755dae37267
Ed Brett <e.j.a.brett@gmail.com>
2018-03-13 14:06:42 +0100

    fix header styles and menu heights

--- app/scripts/App.vue
+++ app/scripts/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <header :class="{'l-header': true, '-over': isExamplePage, '-about': isAboutPage }">
+    <header :class="{'l-header': true, '-map': isExamplePage }">
       <router-view name="HeaderComponent"></router-view>
     </header>
     <main class="l-main">
