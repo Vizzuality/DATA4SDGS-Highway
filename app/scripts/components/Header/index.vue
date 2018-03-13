54acff3aa199228e93f29f4759e19ae38ffbf62b
Ed Brett <e.j.a.brett@gmail.com>
2018-03-13 13:39:54 +0100

    change logos to svgs

--- app/scripts/components/Header/index.vue
+++ app/scripts/components/Header/index.vue
@@ -3,14 +3,12 @@
 <style lang="scss" src="./header-style.scss"></style>
 
 <script>
-import d4sdgLogo from 'static/logos/logo_D4SDG.png';
+import IconComponent from 'components/Icon';
 
 export default {
   name: 'header-component',
-  data() {
-    return {
-      d4sdgLogo,
-    };
-  },
+  components: {
+    IconComponent
+  }
 };
 </script>
