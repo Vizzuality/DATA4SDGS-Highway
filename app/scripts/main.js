e19a6f602e67907a74907fac948f5cb88d520d6c
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2018-07-04 12:58:53 +0200

    adds tooltips

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,8 @@
 import 'styles/lib/foundation.css';
+import 'styles/lib/v-tooltip.css';
 import 'styles/index.scss';
 
+import VTooltip from 'v-tooltip';
 import { sync } from 'vuex-router-sync';
 
 import Vue from 'vue';
@@ -10,6 +12,8 @@ import store from './store';
 import router from './router';
 
 sync(store, router);
+
+Vue.use(VTooltip);
 
 /* eslint-disable no-new */
 new Vue({
